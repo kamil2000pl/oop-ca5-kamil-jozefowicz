32b350028e53d681302b747def555e09214b8398
kamil <kamiljozefowicz@gmail.com>
2021-04-18 00:38:53 +0100

    Added findStudent

--- src/main/java/com/dkit/oopca5/server/StudentDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.dkit.oopca5.server;
-
-import com.dkit.oopca5.Exceptions.DaoException;
-
-public interface StudentDaoInterface {
-//    public boolean registerStudent(Student s) throws DaoException;
-//
-//    public List<Student> findAllStudents() throws DaoException;
-}
